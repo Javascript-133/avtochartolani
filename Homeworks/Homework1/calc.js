a99c11996363a25b1aec97fc9b00a2ba7e8f2e9c
Avto <avtochartolani@gmail.com>
2023-02-02 00:15:56 +0400

    02-feb-23

---
+++ Homeworks/Homework1/calc.js
@@ -0,0 +1,16 @@
+let num1 = parseFloat(prompt("enter number 1: "));
+let num2 = parseFloat(prompt("enter number 2: "));
+let action = prompt("do plus, minus, multiply or divide?");
+let result;
+if (action == "plus") {
+    result = num1 + num2;
+} else if (action == "minus") {
+    result = num1 - num2;
+} else if (action == "multiply") {
+    result = num1 * num2;
+} else if (action == "divide") {
+    result = num1 / num2;
+} else {
+    console.log("error: invalid action");
+}
+console.log(result);
